bc994432ee2e280e3454646fc5b39f07bd3ce2a1
somkiat <somkiat.p@gmail.com>
2014-07-16 08:04:13 +0700

    Add yo client

---
+++ src/yo/yo_client.go
@@ -0,0 +1,56 @@
+package yo
+
+import (
+	"errors"
+	"net/http"
+	"net/url"
+)
+
+var YO_API = "http://api.justyo.co"
+
+type Client struct {
+	Token string
+}
+
+func NewClient(token string) *Client {
+	return &Client{
+		Token: token,
+	}
+}
+
+func (c *Client) YoAll() error {
+	data := url.Values{}
+	data.Set("api_token", c.Token)
+	
+	res, err := http.PostForm(YO_API+"/yoall/", data)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusCreated {
+		return errors.New("Error")
+	}
+
+	return nil
+}
+
+func (c *Client) YoUser(username string) error {
+	data := url.Values{}
+	data.Set("api_token", c.Token)
+	data.Set("username", username)
+
+	res, err := http.PostForm(YO_API+"/yo/", data)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusCreated {
+		return errors.New("Error")
+	}
+
+	return nil
+}
